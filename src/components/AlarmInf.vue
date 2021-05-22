e7d1b7fb7283f089f47f30f34dd698fa2b7c5b99
Aditya Andre <adityaandre33@gmail.com>
2021-05-22 11:59:15 +0700

    feat: CRUD Alarm

--- src/components/AlarmInf.vue
+++ src/components/AlarmInf.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="bg-red row q-pa-sm q-ma-sm">
-  	<div class="col text-center">
+    <div class="col-md-1 text-center">
+     <button @click="$emit('delete-alarm', alarmIndex)"> X </button>
+    </div>
+     <div class="col-md-1 text-center">
+     <button @click="UpdateAlarm(alarmIndex, alarmData.title)"> EDIT </button>
+    </div>
+  	<div class="col text-center text-white">
   		{{alarmData.title}}	
   	</div>
-  	<div class="col text-center">
+  	<div class="col text-center text-white">
   		{{alarmData.caption}}
   	</div>
   </div>
@@ -13,7 +19,18 @@
 export default {
   name: 'Alarminf',
   props: {
-  	alarmData: Object
+  	alarmData: Object,
+    alarmIndex: Number
+  },
+  methods: {
+    UpdateAlarm(alarmIndex, title) {
+      const new_title = prompt ('Judul Baru', title)
+      const params = {
+        index : alarmIndex,
+        title : new_title
+      }
+      this.$emit('update-alarm', params)
+    }
   }
 }
 </script>
--- src/pages/Alarm.vue
+++ src/pages/Alarm.vue
@@ -1,28 +1,58 @@
 <template>
   <div>
+  	<div>
+  		<div class="q-ma-sm h4 text-weight-bold">
+  			Tambah Alarm
+  		</div>
+  		<div class="row">
+  			<input type="text" class="col-sm-4 q-ma-md" v-model="title" placeholder="Judul Alarm">
+	      <q-input filled v-model="caption" mask="time" :rules="['time']">
+	        <template v-slot:append>
+	          <q-icon name="access_time" class="cursor-pointer">
+	            <q-popup-proxy transition-show="scale" transition-hide="scale">
+	              <q-time v-model="caption">
+	                <div class="row items-center justify-end">
+	                  <q-btn v-close-popup label="Close" color="primary" flat />
+	                </div>
+	              </q-time>
+	            </q-popup-proxy>
+	          </q-icon>
+	        </template>
+	      </q-input>
+	      <input type="submit" class="col-sm-1 q-ma-md" label="create" @click="CreateAlarm" >
+      </div>
+  	</div>
   	<AlarmInf 
-  		v-for="alarmData in alarmDatas"
-  		v-bind:key="alarmData.title"
+  		v-for="(alarmData, alarmIndex) in alarmDatas"
+  		v-bind:key="alarmData.id"
   		v-bind:alarmData="alarmData"
+      v-bind:alarmIndex="alarmIndex"
+      @delete-alarm="OnDeleteAlarm"
+      @update-alarm="OnUpdateAlarm"
   	/>
   </div>
 </template>
 
 <script>
+
+import {Quasar, QTime} from 'quasar'
 import AlarmInf from 'components/AlarmInf'
 
 const alarmDatas = [
   {
+    id: '1',
     title: 'TANGI',
     caption: '06:00',
     icon: 'home',
   },
   {
+    id: '2',
     title: 'MASAK',
     caption: '08:00',
     icon: 'alarm',
   },
   {
+    id: '3',
     title: 'TIDUR',
     caption: '21:00',
     icon: 'volunteer_activism',
@@ -33,11 +63,33 @@ export default {
   name: 'Alarm',
   components: {
   	AlarmInf,
+  	QTime
   },
   data () {
   	return {
-  		alarmDatas: alarmDatas
+  		alarmDatas: alarmDatas,
+  		title: null,
+  		caption: null,
   	}
+  },
+  methods: {
+  	CreateAlarm() {
+  		const params = {
+  			title: this.title,
+  			caption: this.caption
+  		}
+  		alarmDatas.push(params)
+  		this.title = ''
+  		this.caption = ''
+  		alert('Tambah Data Sukses')
+  	},
+    OnDeleteAlarm(alarmIndex) {
+      alert('Berhasil Terhapus')
+      this.alarmDatas.splice(alarmIndex, 1)
+    },
+    OnUpdateAlarm(alarmData) {
+      this.alarmDatas[alarmData.index].title = alarmData.title
+  }
   }
 }
 </script>
