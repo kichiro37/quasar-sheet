40198e2a988233a309fc4812d331e0e16d602afb
Aditya Andre <adityaandre33@gmail.com>
2021-05-25 21:19:15 +0700

    feat : search Data

--- src/components/AlarmInf.vue
+++ src/components/AlarmInf.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="bg-red row q-pa-sm q-ma-sm">
+  <div class="bg-info row q-pa-sm q-ma-sm">
     <div class="col-md-1 text-center">
-     <button @click="$emit('delete-alarm', alarmIndex, alarmData.title)"> X </button>
+     <q-btn color="negative" @click="$emit('delete-alarm', alarmIndex, alarmData.title)"> X </q-btn>
     </div>
      <div class="col-md-1 text-center">
-     <button  @click="UpdateAlarm(alarmIndex, alarmData.title)"> EDIT </button>
+     <q-btn color="positive" @click="UpdateAlarm(alarmIndex, alarmData)"> EDIT </q-btn>
     </div>
-  	<div class="col text-center text-white">
+  	<div class="col q-pa-sm text-center text-white">
   		{{alarmData.title}}	
   	</div>
-  	<div class="col text-center text-white">
-  		{{alarmData.caption}}
+  	<div class="col q-pa-sm text-center text-white">
+  		{{alarmData.time}}
   	</div>
-    <div class="col text-center text-white">
+    <div class="col q-pa-sm text-center text-white">
       {{alarmData.qrCode}}
     </div>
   </div>
@@ -26,11 +26,12 @@ export default {
     alarmIndex: Number
   },
   methods: {
-    UpdateAlarm(alarmIndex, title) {
-      const new_title = prompt ('Judul Baru', title)
+    UpdateAlarm(alarmIndex, alarmData) {
       const params = {
         index : alarmIndex,
-        title : new_title
+        title : alarmData.title,
+        time : alarmData.time,
+        qrCode : alarmData.qrCode
       }
       this.$emit('update-alarm', params)
     }
--- src/pages/Alarm.vue
+++ src/pages/Alarm.vue
@@ -1,16 +1,25 @@
 <template>
   <div>
-  	<div>
-  		<div class="q-ma-sm h4 text-weight-bold">
+  	<div class="q-mx-md q-mt-md">
+      <div class="row q-gutter-x-md">
+        <q-input class="col" label="Cari Alarm" placeholder="kode QR" outlined v-model="search">
+          <template v-slot:append>
+            <q-icon name="search" @click="SearchQR"/>
+          </template>
+        </q-input>
+        <q-btn class="col-2" icon="send" color="primary" @click="SearchAlarm"/>
+      </div>
+      <hr class="q-my-lg" />
+  		<div class="text-h5 text-weight-bold q-mb-md">
   			Tambah Alarm
   		</div>
-  		<div class="row">
-  			<input type="text" class="col-sm-4 q-ma-md" v-model="title" placeholder="Judul Alarm">
-	      <q-input filled v-model="caption" mask="time" :rules="['time']">
+  		<div class="row q-gutter-x-md">
+  			<q-input type="text" class="col" v-model="title" placeholder="Judul Alarm" outlined/>
+	      <q-input class="col-4" filled v-model="time" mask="time" :rules="['time']">
 	        <template v-slot:append>
 	          <q-icon name="access_time" class="cursor-pointer">
 	            <q-popup-proxy transition-show="scale" transition-hide="scale">
-	              <q-time v-model="caption">
+	              <q-time v-model="time">
 	                <div class="row items-center justify-end">
 	                  <q-btn v-close-popup label="Close" color="primary" flat />
 	                </div>
@@ -20,12 +29,14 @@
 	        </template>
 	      </q-input>
       </div>
-      <div class="row">
-        <q-btn class="col" label="OpenQr" @click="OpenQr"/>
+      <div class="row q-gutter-x-md">
         <q-input btn class="col" label="output" disable v-model="qrCode" />
+         <q-btn class="col" color="positive" label="OpenQr" @click="OpenQr"/>
       </div>
       <div class="row">
-        <q-btn color="primary" class="col q-ma-md" label="create" @click="CreateAlarm" />
+        <q-btn v-if="alarmIndex != null" color="positive" class="col q-ma-md" label="update" @click="UpdateBaru(alarmDatas, alarmIndex)" 
+        />
+        <q-btn v-else color="primary" class="col q-ma-md" label="create" @click="CreateAlarm" />
       </div>
   	</div>
   	<AlarmInf 
@@ -48,20 +59,57 @@ export default {
   name: 'Alarm',
   components: {
   	AlarmInf,
-  	QTime
+  	QTime,
   },
   data () {
   	return {
   		alarmDatas: [],
   		title: null,
-  		caption: null,
-      qrCode: null
+  		time: null,
+      qrCode: null,
+      search: null,
+      alarmIndex: null
     }
   },
   created() {
     this.GetAlarmDatas()
   },
   methods: {
+    ScanQR() {
+      return new Promise((resolve, reject) => {
+        cordova.plugins.barcodeScanner.scan(
+          result => {
+            resolve(result)
+          },
+          error => {
+            reject(error)
+          }
+        )
+      })
+    },
+    async SearchQR() {
+      this.search = await this.ScanQR()
+      this.search = this.search.text
+    },
+    async OpenQr() {
+      this.qrCode = await this.ScanQR()
+      this.qrCode = this.qrCode.text
+    },
+    SearchAlarm() {
+      const alarmData = this.alarmDatas.filter((alarmData, alarmIndex) => {
+        const IS_FOUND = alarmData.qrCode === this.search 
+        if (IS_FOUND) this.alarmIndex = alarmIndex
+        return IS_FOUND
+      })
+
+      if(!alarmData.length) alert('Search not found')
+      else {
+        this.title = alarmData[0].title
+        this.time = alarmData[0].time
+        this.qrCode = alarmData[0].qrCode
+      }
+
+    },
     GetAlarmDatas() {
       this.alarmDatas = LocalStorage.getItem('alarmDatas') || []
     },
@@ -71,13 +119,13 @@ export default {
   	CreateAlarm() {
   		const params = {
   			title: this.title,
-  			caption: this.caption,
+  			time: this.time,
         qrCode: this.qrCode
   		}
   		this.alarmDatas.push(params)
       this.SaveStorage()
   		this.title = ''
-  		this.caption = ''
+  		this.time = '00:00'
       this.qrCode = ''
   		alert('Tambah Data Sukses')
   	},
@@ -87,23 +135,27 @@ export default {
       this.SaveStorage()
     },
     OnUpdateAlarm(alarmData) {
-      this.alarmDatas[alarmData.index].title = alarmData.title
-      alert('Berhasil Edit')
+      this.alarmIndex = alarmData.index
+      this.title = alarmData.title
+      this.time = alarmData.time
+      this.qrCode = alarmData.qrCode
+    },
+    UpdateBaru(alarmDatas, alarmIndex) {
+      console.log(alarmDatas)
+      console.log('this alarm datas', this.alarmDatas)
+      console.log('this alarm index', alarmIndex)
+      console.log('this title', this.title)
+      this.alarmDatas[alarmIndex].title = this.title
+      this.alarmDatas[alarmIndex].time = this.time
+      this.alarmDatas[alarmIndex].qrCode = this.qrCode
+      alert('Berhasil Update')
       this.SaveStorage()
-    },
-    OpenQr() {
-      cordova.plugins.barcodeScanner.scan(
-        result => {
-          alert(`SCAN SUKSES ${JSON.stringify(result)}`)
-          console.log('SCAN SUKSES', result)
-          this.qrCode = result.text
-        },
-        error => {
-          alert (`SCAN ERROR ${error}`)
-          console.log('SCAN ERROR', error)
-        }
-      )
-    },
+
+      this.title = ''
+      this.time = '00:00'
+      this.qrCode = ''
+      this.alarmIndex = null
+    }
   }
 }
 </script>
